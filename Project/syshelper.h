52a3bb81d56f1cff45b7dca8554ef5652f20ea91
Baylee Sims <simsbk@g.cofc.edu>
2017-12-04 02:51:59 -0500

    Add files via upload

---
+++ Project/syshelper.h
@@ -0,0 +1,45 @@
+
+#define SUCCESSFUL 0
+#define ERROR -1
+
+#define TRUE 1
+#define FALSE 0
+
+#define DEBUG 1
+
+/**
+-----------------------------------------------------------------------
+*/
+int equal_to( char* str1, char* str2 );
+
+/** 
+-----------------------------------------------------------------------
+	This function is used to compare non case-sensitive string
+	values
+-----------------------------------------------------------------------
+*/
+
+void reformat_string(char *src, char *dst);
+/** 
+-----------------------------------------------------------------------
+	This function is used to convert a string into lower-case,
+	non-punctuationed version
+-----------------------------------------------------------------------
+*/
+
+int parsing(char* line, int exit);
+
+/** 
+-----------------------------------------------------------------------
+	This function is used to parse the commands entered into the
+	terminal
+-----------------------------------------------------------------------
+*/
+
+int shellHelper( int argc, char** argv );
+
+/** 
+-----------------------------------------------------------------------
+	This function is used to run syshelper.c
+-----------------------------------------------------------------------
+*/
